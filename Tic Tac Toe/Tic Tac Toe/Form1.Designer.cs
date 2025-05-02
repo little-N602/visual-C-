c0cf9b561f4db010d8119b23d6a6a21fcb047cae
little-N602 <lui2175264@maricopa.edu>
2025-05-02 11:42:38 -0700

    Add files via upload

---
+++ Tic Tac Toe/Tic Tac Toe/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.r1c1 = new System.Windows.Forms.Label();
+            this.r1c2 = new System.Windows.Forms.Label();
+            this.r1c3 = new System.Windows.Forms.Label();
+            this.r2c1 = new System.Windows.Forms.Label();
+            this.r3c1 = new System.Windows.Forms.Label();
+            this.r2c2 = new System.Windows.Forms.Label();
+            this.r2c3 = new System.Windows.Forms.Label();
+            this.r3c2 = new System.Windows.Forms.Label();
+            this.r3c3 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // r1c1
+            // 
+            this.r1c1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.r1c1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r1c1.Location = new System.Drawing.Point(12, 9);
+            this.r1c1.Name = "r1c1";
+            this.r1c1.Size = new System.Drawing.Size(35, 35);
+            this.r1c1.TabIndex = 0;
+            this.r1c1.Click += new System.EventHandler(this.r1c1_Click);
+            // 
+            // r1c2
+            // 
+            this.r1c2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.r1c2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r1c2.Location = new System.Drawing.Point(62, 9);
+            this.r1c2.Name = "r1c2";
+            this.r1c2.Size = new System.Drawing.Size(35, 35);
+            this.r1c2.TabIndex = 1;
+            this.r1c2.Click += new System.EventHandler(this.r1c2_Click);
+            // 
+            // r1c3
+            // 
+            this.r1c3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.r1c3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r1c3.Location = new System.Drawing.Point(114, 9);
+            this.r1c3.Name = "r1c3";
+            this.r1c3.Size = new System.Drawing.Size(35, 35);
+            this.r1c3.TabIndex = 2;
+            this.r1c3.Click += new System.EventHandler(this.r1c3_Click);
+            // 
+            // r2c1
+            // 
+            this.r2c1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.r2c1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r2c1.Location = new System.Drawing.Point(12, 59);
+            this.r2c1.Name = "r2c1";
+            this.r2c1.Size = new System.Drawing.Size(35, 35);
+            this.r2c1.TabIndex = 3;
+            this.r2c1.Click += new System.EventHandler(this.r2c1_Click);
+            // 
+            // r3c1
+            // 
+            this.r3c1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.r3c1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r3c1.Location = new System.Drawing.Point(12, 107);
+            this.r3c1.Name = "r3c1";
+            this.r3c1.Size = new System.Drawing.Size(35, 35);
+            this.r3c1.TabIndex = 4;
+            this.r3c1.Click += new System.EventHandler(this.r3c1_Click);
+            // 
+            // r2c2
+            // 
+            this.r2c2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.r2c2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r2c2.Location = new System.Drawing.Point(62, 59);
+            this.r2c2.Name = "r2c2";
+            this.r2c2.Size = new System.Drawing.Size(35, 35);
+            this.r2c2.TabIndex = 5;
+            this.r2c2.Click += new System.EventHandler(this.r2c2_Click);
+            // 
+            // r2c3
+            // 
+            this.r2c3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.r2c3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r2c3.Location = new System.Drawing.Point(114, 59);
+            this.r2c3.Name = "r2c3";
+            this.r2c3.Size = new System.Drawing.Size(35, 35);
+            this.r2c3.TabIndex = 6;
+            this.r2c3.Click += new System.EventHandler(this.r2c3_Click);
+            // 
+            // r3c2
+            // 
+            this.r3c2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.r3c2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r3c2.Location = new System.Drawing.Point(62, 107);
+            this.r3c2.Name = "r3c2";
+            this.r3c2.Size = new System.Drawing.Size(35, 35);
+            this.r3c2.TabIndex = 7;
+            this.r3c2.Click += new System.EventHandler(this.r3c2_Click);
+            // 
+            // r3c3
+            // 
+            this.r3c3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.r3c3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r3c3.Location = new System.Drawing.Point(114, 107);
+            this.r3c3.Name = "r3c3";
+            this.r3c3.Size = new System.Drawing.Size(35, 35);
+            this.r3c3.TabIndex = 8;
+            this.r3c3.Click += new System.EventHandler(this.r3c3_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Location = new System.Drawing.Point(12, 162);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(137, 23);
+            this.lblResult.TabIndex = 9;
+            this.lblResult.Text = "Player X\'s turn";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(93, 197);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(56, 23);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Enabled = false;
+            this.btnNewGame.Location = new System.Drawing.Point(12, 197);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(75, 23);
+            this.btnNewGame.TabIndex = 12;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.newGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(164, 239);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.r3c3);
+            this.Controls.Add(this.r3c2);
+            this.Controls.Add(this.r2c3);
+            this.Controls.Add(this.r2c2);
+            this.Controls.Add(this.r3c1);
+            this.Controls.Add(this.r2c1);
+            this.Controls.Add(this.r1c3);
+            this.Controls.Add(this.r1c2);
+            this.Controls.Add(this.r1c1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label r1c1;
+        private System.Windows.Forms.Label r1c2;
+        private System.Windows.Forms.Label r1c3;
+        private System.Windows.Forms.Label r2c1;
+        private System.Windows.Forms.Label r3c1;
+        private System.Windows.Forms.Label r2c2;
+        private System.Windows.Forms.Label r2c3;
+        private System.Windows.Forms.Label r3c2;
+        private System.Windows.Forms.Label r3c3;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnNewGame;
+    }
+}
+
---
+++ Tic Tac Toe/Tic Tac Toe/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Declare a 3x3 char array called ticTacToe
+        char playerTurn = 'X';
+        char [,] ticTacToe = new char[3, 3];
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clear()
+        {            
+            Array.Clear(ticTacToe, 0, 9);
+            r1c1.Text = "";
+            r1c2.Text = "";
+            r1c3.Text = "";
+            r2c1.Text = "";
+            r2c2.Text = "";
+            r2c3.Text = "";
+            r3c1.Text = "";
+            r3c2.Text = "";
+            r3c3.Text = "";
+            lblResult.Text = "Player X's turn";
+            playerTurn = 'X';
+        }
+
+        
+
+        private void displayArray()
+        {
+            r1c1.Text = ticTacToe[0, 0].ToString();
+            r1c2.Text = ticTacToe[0, 1].ToString();
+            r1c3.Text = ticTacToe[0, 2].ToString();
+            r2c1.Text = ticTacToe[1, 0].ToString();
+            r2c2.Text = ticTacToe[1, 1].ToString();
+            r2c3.Text = ticTacToe[1, 2].ToString();
+            r3c1.Text = ticTacToe[2, 0].ToString();
+            r3c2.Text = ticTacToe[2, 1].ToString();
+            r3c3.Text = ticTacToe[2, 2].ToString();
+            //Add six more lines of code to output the correct array value into the correct label.
+            //I've already done Row 1 for you.  You need to complete rows 2 and 3.
+        }
+
+        private void checkForWin()
+        {
+            if (((ticTacToe[0, 0] == 'X') && (ticTacToe[0, 1] == 'X') && (ticTacToe[0, 2] == 'X')) ||
+            ((ticTacToe[1, 0] == 'X') && (ticTacToe[1, 1] == 'X') && (ticTacToe[1, 2] == 'X')) ||
+            ((ticTacToe[2, 0] == 'X') && (ticTacToe[2, 1] == 'X') && (ticTacToe[2, 2] == 'X')) ||
+            ((ticTacToe[0, 0] == 'X') && (ticTacToe[1, 0] == 'X') && (ticTacToe[2, 0] == 'X')) ||
+            ((ticTacToe[0, 1] == 'X') && (ticTacToe[1, 1] == 'X') && (ticTacToe[2, 1] == 'X')) ||
+            ((ticTacToe[0, 2] == 'X') && (ticTacToe[1, 2] == 'X') && (ticTacToe[2, 2] == 'X')) ||
+            ((ticTacToe[0, 0] == 'X') && (ticTacToe[1, 1] == 'X') && (ticTacToe[2, 2] == 'X')) ||
+            ((ticTacToe[0, 2] == 'X') && (ticTacToe[1, 1] == 'X') && (ticTacToe[2, 0] == 'X'))
+            //Add five more lines of code to validate for a Team X vidtory in column 1, column 2, column 3, 
+            //diagonal top left to bottom right, and diagonal top right to bottom left
+            )
+            {
+                lblResult.Text = "X wins!";
+                btnNewGame.Enabled = true;
+            }
+
+            else if (((ticTacToe[0, 0] == 'O') && (ticTacToe[0, 1] == 'O') && (ticTacToe[0, 2] == 'O')) ||
+            ((ticTacToe[1, 0] == 'O') && (ticTacToe[1, 1] == 'O') && (ticTacToe[1, 2] == 'O')) ||
+            ((ticTacToe[2, 0] == 'O') && (ticTacToe[2, 1] == 'O') && (ticTacToe[2, 2] == 'O')) ||
+            ((ticTacToe[0, 0] == 'O') && (ticTacToe[1, 0] == 'O') && (ticTacToe[2, 0] == 'O')) ||
+            ((ticTacToe[0, 1] == 'O') && (ticTacToe[1, 1] == 'O') && (ticTacToe[2, 1] == 'O')) ||
+            ((ticTacToe[0, 2] == 'O') && (ticTacToe[1, 2] == 'O') && (ticTacToe[2, 2] == 'O')) ||
+            ((ticTacToe[0, 0] == 'O') && (ticTacToe[1, 1] == 'O') && (ticTacToe[2, 2] == 'O')) ||
+            ((ticTacToe[0, 2] == 'O') && (ticTacToe[1, 1] == 'O') && (ticTacToe[2, 0] == 'O')))
+            {
+                lblResult.Text = "O wins!";
+                btnNewGame.Enabled = true;
+            }
+
+            else if (((ticTacToe[0, 0] == 'X') || (ticTacToe[0, 0] == 'O')) &&
+            ((ticTacToe[0, 1] == 'X') || (ticTacToe[0, 1] == 'O')) &&
+            ((ticTacToe[0, 2] == 'X') || (ticTacToe[0, 2] == 'O')) &&
+            ((ticTacToe[1, 0] == 'X') || (ticTacToe[1, 0] == 'O')) &&
+            ((ticTacToe[1, 1] == 'X') || (ticTacToe[1, 1] == 'O')) &&
+            ((ticTacToe[1, 2] == 'X') || (ticTacToe[1, 2] == 'O')) &&
+            ((ticTacToe[2, 0] == 'X') || (ticTacToe[2, 0] == 'O')) &&
+            ((ticTacToe[2, 1] == 'X') || (ticTacToe[2, 1] == 'O')) &&
+            ((ticTacToe[2, 2] == 'X') || (ticTacToe[2, 2] == '0'))
+            //Add three more lines of code to validate whether each of 
+            //the three boxes in the bottom row have either 'X' or 'O' in it
+            )
+            {
+                lblResult.Text = "Tie game!";
+                btnNewGame.Enabled = true;
+            }
+        }
+
+        private void newGame_Click(object sender, EventArgs e)
+        {
+            clear();
+            btnNewGame.Enabled = false;
+        }
+
+        private void r1c1_Click(object sender, EventArgs e)
+        {
+            if (playerTurn == 'X')
+            {
+                ticTacToe[0, 0] = 'X';
+                lblResult.Text = "Player O's turn";
+                playerTurn = 'O';
+            }
+            else if (playerTurn == 'O')
+            {
+                ticTacToe[0, 0] = 'O';
+                lblResult.Text = "Player X's turn";
+                playerTurn = 'X';
+            }
+
+            displayArray();
+            checkForWin();
+        }
+
+        private void r1c2_Click(object sender, EventArgs e)
+        {
+
+            if (playerTurn == 'X')
+            {
+                ticTacToe[0, 1] = 'X';
+                lblResult.Text = "Player O's turn";
+                playerTurn = 'O';
+            }
+            else if (playerTurn == 'O')
+            {
+                ticTacToe[0, 1] = 'O';
+                lblResult.Text = "Player X's turn";
+                playerTurn = 'X';
+            }
+
+            displayArray();
+            checkForWin();
+        }
+
+        private void r1c3_Click(object sender, EventArgs e)
+        {
+
+            if (playerTurn == 'X')
+            {
+                ticTacToe[0, 2] = 'X';
+                lblResult.Text = "Player O's turn";
+                playerTurn = 'O';
+            }
+            else if (playerTurn == 'O')
+            {
+                ticTacToe[0, 2] = 'O';
+                lblResult.Text = "Player X's turn";
+                playerTurn = 'X';
+            }
+
+            displayArray();
+            checkForWin();
+        }
+
+        private void r2c1_Click(object sender, EventArgs e)
+        {
+            if (playerTurn == 'X')
+            {
+                ticTacToe[1,0] = 'X';
+                lblResult.Text = "Player O's turn";
+                playerTurn = 'O';
+            }
+            else if (playerTurn == 'O')
+            {
+                ticTacToe[1, 0] = 'O';
+                lblResult.Text = "Player X's turn";
+                playerTurn = 'X';
+            }
+
+            displayArray();
+            checkForWin();
+        }
+
+        private void r2c2_Click(object sender, EventArgs e)
+        {
+            if (playerTurn == 'X')
+            {
+                ticTacToe[1,1] = 'X';
+                lblResult.Text = "Player O's turn";
+                playerTurn = 'O';
+            }
+            else if (playerTurn == 'O')
+            {
+                ticTacToe[1, 1] = 'O';
+                lblResult.Text = "Player X's turn";
+                playerTurn = 'X';
+            }
+
+            displayArray();
+            checkForWin();
+        }
+
+        private void r2c3_Click(object sender, EventArgs e)
+        {
+            if (playerTurn == 'X')
+            {
+                ticTacToe[1, 2] = 'X';
+                lblResult.Text = "Player O's turn";
+                playerTurn = 'O';
+            }
+            else if (playerTurn == 'O')
+            {
+                ticTacToe[1, 2] = 'O';
+                lblResult.Text = "Player X's turn";
+                playerTurn = 'X';
+            }
+
+            displayArray();
+            checkForWin();
+        }
+
+        private void r3c1_Click(object sender, EventArgs e)
+        {
+            if (playerTurn == 'X')
+            {
+                ticTacToe[2,0] = 'X';
+                lblResult.Text = "Player O's turn";
+                playerTurn = 'O';
+            }
+            else if (playerTurn == 'O')
+            {
+                ticTacToe[2, 0] = 'O';
+                lblResult.Text = "Player X's turn";
+                playerTurn = 'X';
+            }
+
+            displayArray();
+            checkForWin();
+        }
+
+        private void r3c2_Click(object sender, EventArgs e)
+        {
+            if (playerTurn == 'X')
+            {
+                ticTacToe[2, 1] = 'X';
+                lblResult.Text = "Player O's turn";
+                playerTurn = 'O';
+            }
+            else if (playerTurn == 'O')
+            {
+                ticTacToe[2, 1] = 'O';
+                lblResult.Text = "Player X's turn";
+                playerTurn = 'X';
+            }
+
+            displayArray();
+            checkForWin();
+        }
+
+        private void r3c3_Click(object sender, EventArgs e)
+        {
+            if (playerTurn == 'X')
+            {
+                ticTacToe[2, 2] = 'X';
+                lblResult.Text = "Player O's turn";
+                playerTurn = 'O';
+            }
+            else if (playerTurn == 'O')
+            {
+                ticTacToe[2, 2] = 'O';
+                lblResult.Text = "Player X's turn";
+                playerTurn = 'X';
+            }
+
+            displayArray();
+            checkForWin();
+        }
+    }
+}
