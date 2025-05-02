4159ea3398ae16cc811e3ab8302a88928087daae
little-N602 <lui2175264@maricopa.edu>
2025-05-02 11:42:09 -0700

    Add files via upload

---
+++ Word Counter/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Word_Counter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.count = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.total = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // count
+            // 
+            this.count.Location = new System.Drawing.Point(272, 134);
+            this.count.Name = "count";
+            this.count.Size = new System.Drawing.Size(75, 23);
+            this.count.TabIndex = 0;
+            this.count.Text = "count";
+            this.count.UseVisualStyleBackColor = true;
+            this.count.Click += new System.EventHandler(this.count_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(353, 134);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(75, 23);
+            this.exit.TabIndex = 1;
+            this.exit.Text = "exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(297, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "number of words";
+            // 
+            // total
+            // 
+            this.total.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.total.Location = new System.Drawing.Point(146, 134);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(100, 23);
+            this.total.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(175, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(207, 33);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Word Counter";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 196);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.count);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button count;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label total;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Word Counter/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Word_Counter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void count_Click(object sender, EventArgs e)
+        {
+            
+                string word = textBox1.Text.Trim();
+                int count = countword(word);
+            if (count > 0)
+            {
+                total.Text = count.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please enter text into the text box.", "Word Count");
+            }
+        }
+        private int countword(string word)
+        {
+            if (string.IsNullOrEmpty(word))
+            {
+                return 0;
+            }
+            string[] allwords = word.Split(' ');
+            return allwords.Length;
+        }
+    }
+}
